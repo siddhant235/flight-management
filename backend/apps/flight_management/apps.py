839805c49d809282c9895b3f91ee4d67f853d0d6
siddhant235 <siddhant235@gmail.com>
2025-03-09 11:11:50 +0530

    deleted some files

--- backend/apps/flight_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlightManagementConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.flight_management"
-    label = "flight_management"
